c4d3bc9dcf2dcde0d1ff42d4d932c6831e8a6d38
ShacharHarel <shacharharell@gmail.com>
2026-08-13 08:42:33 +0300

    Add content model

---
+++ models/Content.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const contentSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['movie', 'series'],
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 10,
+        default: 0
+    },
+    videoUrl: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Content', contentSchema);
